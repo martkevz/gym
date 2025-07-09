5158761a99a023603d0815eb70c5d36708410f65
rp23005 <rp23005@ues.edu.sv>
2025-07-09 10:21:52 -0600

    Starting UsuarioPorMembresia.java class

---
+++ src/main/java/com/app/gym/modelos/UsuarioPorMembresia.java
@@ -0,0 +1,13 @@
+package com.app.gym.modelos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.Immutable;
+
+@Entity
+@Immutable
+@Table(name = "usuarios_por_membresia")
+public class UsuarioPorMembresia {
+    
+    priv
+}
